5fd0e2683fe4bd80669fa6693b06a97d686830e8
FutureIntelligenceDxB <hello@futureintelligence.me>
2022-06-17 16:24:34 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/bizzcard_stub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bizzcard_stub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[500],
+        body: Container(),
+      ),
+    ),
+  );
+}
